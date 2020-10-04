455a45d02ddf532ac3b28318ad40850383d98007
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2020-10-04 21:07:40 +0200

    cmd: pass libvirtURI from cmd

--- cmd/root.go
+++ cmd/root.go
@@ -7,15 +7,15 @@ import (
 )
 
 var (
-	logFile string
-	log     bool
-	rootCmd = &cobra.Command{
+	logFile, libvirtURI string
+	log                 bool
+	rootCmd             = &cobra.Command{
 		Use:   "virtcurse",
 		Short: "manage your hypervisor from a curses interface",
 		Long: `manage your hypervisor from a curses interface using Libvirt APIs
 and supported drivers: https://libvirt.org/drivers.html`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return components.Execute()
+			return components.Execute(cmd)
 		},
 	}
 )
@@ -31,4 +31,5 @@ func init() {
 	)
 	rootCmd.PersistentFlags().StringVar(&logFile, "log-file", "./virtcurse.log", "path to store the logs")
 	rootCmd.PersistentFlags().BoolVar(&log, "log", false, "enable logging in the `log-file`")
+	rootCmd.PersistentFlags().StringVar(&libvirtURI, "libvirt-uri", "qemu:///system", "virsh connect URI")
 }
