ca5dc70a3ebde7e31f7c1e53ebfbbf955acd1b45
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2020-10-04 21:08:36 +0200

    components: add root comp
    this root component will hold all the components

---
+++ components/root.go
@@ -0,0 +1,42 @@
+package components
+
+import (
+	ui "github.com/gizak/termui/v3"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+
+	"github.com/tormath1/virtcurse/driver/libvirt"
+)
+
+func Execute(cmd *cobra.Command) error {
+	libvirtURI, err := cmd.Flags().GetString("libvirt-uri")
+	if err != nil {
+		return errors.Wrap(err, "unable to get libvirt-uri")
+	}
+
+	d, err := libvirt.NewLibvirt(libvirtURI)
+	if err != nil {
+		return errors.Wrap(err, "unable to init libvirt client")
+	}
+
+	if err := ui.Init(); err != nil {
+		return errors.Wrap(err, "unable to init UI")
+	}
+	defer ui.Close()
+
+	components := []ui.Drawable{
+		NewListDomains(d),
+	}
+
+	for _, component := range components {
+		ui.Render(component)
+	}
+
+	for e := range ui.PollEvents() {
+		if e.Type == ui.KeyboardEvent {
+			break
+		}
+	}
+
+	return nil
+}
