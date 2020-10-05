7fc5a24a24910e86299559e16875229ce53e3d6f
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2020-10-05 22:06:51 +0200

    driver/options: add doc

--- driver/options.go
+++ driver/options.go
@@ -1,15 +1,22 @@
 package driver
 
+// ListFlag is an optional flag
+// to pass to the ListDomain method
 type ListFlag int
 
 const (
+	// All lists all the domains
 	All ListFlag = iota
+	// Shutdown lists only shutdowned domains
 	Shutdown
+	// Active lists only active domains
 	Active
 )
 
 // Options is the helper to handle
 // driver options for requests
 type Options struct {
+	// ListOptions is the options to pass
+	// to the ListDomain methods
 	ListOptions ListFlag
 }
