cbac34cfe2ccd61ba105f51ab493a1483ee7caeb
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2020-10-05 21:49:53 +0200

    driver: add XML description

--- driver/domain.go
+++ driver/domain.go
@@ -4,5 +4,8 @@ package driver
 // representing domains
 type Domain interface {
 	// Name is the name of the domain
-	Name() string
+	Name() (string, error)
+
+	// XML returns the XML description of the domain
+	XML() (string, error)
 }
--- driver/libvirt/domain.go
+++ driver/libvirt/domain.go
@@ -1,7 +1,27 @@
 package libvirt
 
-type Domain struct{ name string }
+import (
+	libvirt "github.com/libvirt/libvirt-go"
+	"github.com/pkg/errors"
+)
 
-func (d Domain) Name() string {
-	return d.name
+type Domain struct {
+	dom libvirt.Domain
+}
+
+func (d Domain) Name() (string, error) {
+	name, err := d.dom.GetName()
+	if err != nil {
+		// TODO: Should we skip this error and use a default name
+		return "", errors.Wrap(err, "unable to get domain's name")
+	}
+	return name, nil
+}
+
+func (d Domain) XML() (string, error) {
+	xml, err := d.dom.GetXMLDesc(0)
+	if err != nil {
+		return "", errors.Wrap(err, "unable to get xml description")
+	}
+	return xml, nil
 }
